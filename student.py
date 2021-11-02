9e4e7339a568d4d13fd189b6462bf4e5679c0c21
Kaiyi Yang <91563502+davidy2024@users.noreply.github.com>
2021-11-02 18:58:30 +0000

    text

--- student.py
+++ student.py
@@ -59,7 +59,7 @@ class Piggy(PiggyParent):
     def yang(self):
       self.deg_fwd(360)
       time.sleep(3)
-      self.right(primary = 20, counter =-20)
+      self.right(primary = 40, counter =-40)
       self.sleep(1.2)
       self.stop()
       
