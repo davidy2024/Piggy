bdd3186d8a6e045495bdec8d50b7b3048f38d7c6
Kaiyi Yang <91563502+davidy2024@users.noreply.github.com>
2021-11-02 18:40:09 +0000

    added movement

--- student.py
+++ student.py
@@ -57,7 +57,7 @@ class Piggy(PiggyParent):
     ****************
     '''
     def yang(self):
-      pass
+      deg_fwd(360)
       
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
