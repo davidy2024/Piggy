3a7c47b5099659cd528fafa1f092517e152d3ba3
Kaiyi Yang <91563502+davidy2024@users.noreply.github.com>
2021-12-07 20:07:08 +0000

    fix

--- student.py
+++ student.py
@@ -80,31 +80,31 @@ class Piggy(PiggyParent):
           self.fwd()
           time.sleep(1) 
           if self.read_distance() < 150:
-          self.stop()
-          self.servo(self.MIDPOINT + 300)
-          time.sleep(1)
-          left = self.read_distance()
-          self.servo(self.MIDPOINT - 300)
-          time.sleep(1)
-          right = self.read_distance()
-        if left > right :
-          self.left(primary = 43, counter =-40)
-          time.sleep(1)
-          self.fwd()
-          time.sleep(2)
-          self.right(primary = 41, counter =-37)
-          time.sleep(1)
-          self.servo(self.MIDPOINT)
+            self.stop()
+            self.servo(self.MIDPOINT + 300)
+            time.sleep(1)
+            left = self.read_distance()
+            self.servo(self.MIDPOINT - 300)
+            time.sleep(1)
+            right = self.read_distance()
+              if left > right :
+                self.left(primary = 43, counter =-40)
+                time.sleep(1)
+                self.fwd()
+                time.sleep(2)
+                self.right(primary = 41, counter =-37)
+                time.sleep(1)
+                self.servo(self.MIDPOINT)
             
 
-        elif right > left :
-          self.right(primary = 43, counter =-40)
-          time.sleep(1)
-          self.fwd()
-          time.sleep(2)
-          self.left(primary = 41, counter =-37)
-          time.sleep(1)
-          self.servo(self.MIDPOINT)
+              elif right > left :
+                self.right(primary = 43, counter =-40)
+                time.sleep(1)
+                self.fwd()
+                time.sleep(2)
+                self.left(primary = 41, counter =-37)
+                time.sleep(1)
+                self.servo(self.MIDPOINT)
 
 
         
